56f8124932b085f9f5e2e71f81868943fb47c085
MD Anas Khan <akhananas02@gmail.com>
2024-12-18 19:10:10 +0530

    Added app.keys configuration

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['myRandomKey1', 'myRandomKey2', 'myRandomKey3']), // Add your keys here
   },
 });
